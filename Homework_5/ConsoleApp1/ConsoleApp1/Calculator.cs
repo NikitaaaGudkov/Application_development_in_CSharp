37c6d868ba38ebbc973970e40665056ac7821ecf
NikitaaaGudkov <nikita.gudkov.2015@mail.ru>
2024-01-27 21:55:43 +0300

    Homework 5 done

---
+++ Homework_5/ConsoleApp1/ConsoleApp1/Calculator.cs
@@ -0,0 +1,69 @@
+﻿namespace ConsoleApp1
+{
+    internal class Calculator
+    {
+        public int result;
+        public event EventHandler<CalculatorArgs> ShowResult = null!;
+
+        public Stack<int> stack;
+        public Calculator()
+        {
+            result = 0;
+            stack = new Stack<int>();
+        }
+
+        public void Calculation(string? symbol, string? value)
+        {
+            if (!int.TryParse(value, out int number))
+            {
+                Console.WriteLine("Ошибка! Вы ввели не число");
+            }
+            stack.Push(result);
+            switch (symbol)
+            {
+                case "+":
+                    {
+                        result += number;
+                        break;
+                    }
+                case "-":
+                    {
+                        result -= number;
+                        break;
+                    }
+                case "*":
+                    {
+                        result *= number;
+                        break;
+                    }
+                case "/":
+                    {
+                        result /= number;
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Ошибка! Неизвестная операция");
+                        break;
+                    }
+            }
+            ShowResult.Invoke(this, new CalculatorArgs(result));
+        }
+
+
+        public void CancelLastCalculation()
+        {
+            result = stack.Count > 0 ? stack.Pop() : 0;
+            ShowResult.Invoke(this, new CalculatorArgs(result));
+        }
+    }
+
+    public class CalculatorArgs : EventArgs
+    {
+        public int answer;
+        public CalculatorArgs(int value)
+        {
+            answer = value;
+        }
+    }
+}
---
+++ Homework_5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Доработайте программу калькулятор реализовав выбор действий и вывод результатов на экран в цикле так 
+чтобы калькулятор мог работать до тех пор пока пользователь не нажмет отмена или введёт пустую строку.
+ */
+
+
+using ConsoleApp1;
+
+Calculator calculator = new Calculator();
+calculator.ShowResult += PrintResult;
+while (true)
+{
+    Console.Write("Введите операцию: +, -, *, /.\n" +
+        "Для отмены крайнего действия напишите \"возврат\".\n" +
+        "Для завершения программы напишите \"отмена\" или введите пустую строку\t");
+    string? operation = Console.ReadLine();
+    if(operation == string.Empty || operation == "отмена")
+    {
+        Console.WriteLine("Завершение работы...");
+        break;
+    }
+    else if (operation == "возврат")
+    {
+        calculator.CancelLastCalculation();
+    }
+    else
+    {
+        Console.Write("Введите число: ");
+        string? number = Console.ReadLine();
+        calculator.Calculation(operation, number);
+    }
+    Console.WriteLine();
+}
+calculator.ShowResult -= PrintResult;
+
+
+void PrintResult(object? sender, CalculatorArgs e)
+{
+    Console.WriteLine($"Текущий результат равен = {e.answer}");
+}
