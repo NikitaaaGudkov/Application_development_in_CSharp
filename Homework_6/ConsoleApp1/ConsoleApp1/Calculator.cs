8b94d5b4118ce56fd1bc56e0a38ac1654221fbe9
NikitaaaGudkov <nikita.gudkov.2015@mail.ru>
2024-02-01 18:33:29 +0300

    Homework 6 done

---
+++ Homework_6/ConsoleApp1/ConsoleApp1/Calculator.cs
@@ -0,0 +1,82 @@
+﻿namespace ConsoleApp1
+{
+    internal class Calculator
+    {
+        public double result;
+        public event EventHandler<CalculatorArgs> ShowResult = null!;
+
+        public Stack<double> stack;
+        public Calculator()
+        {
+            result = 0;
+            stack = new Stack<double>();
+        }
+
+        public void Calculation(string? symbol, string? value)
+        {
+            value = value?.Replace(".", ",");
+            if (!double.TryParse(value, out double number))
+            {
+                Console.WriteLine("Ошибка! Вы ввели не число");
+            }
+            else
+            {
+                stack.Push(result);
+                switch (symbol)
+                {
+                    case "+":
+                        {
+                            result += number;
+                            break;
+                        }
+                    case "-":
+                        {
+                            result -= number;
+                            break;
+                        }
+                    case "*":
+                        {
+                            result *= number;
+                            break;
+                        }
+                    case "/":
+                        {
+                            if (number == 0)
+                            {
+                                Console.WriteLine("Вы ввели 0. На ноль делить нельзя!");
+                                stack.Pop();
+                            }
+                            else
+                            {
+                                result /= number;
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Ошибка! Неизвестная операция");
+                            break;
+                        }
+                }
+                result = Math.Round(result, 3, MidpointRounding.AwayFromZero);
+            }    
+            ShowResult.Invoke(this, new CalculatorArgs(result));
+        }
+
+
+        public void CancelLastCalculation()
+        {
+            result = stack.Count > 0 ? stack.Pop() : 0;
+            ShowResult.Invoke(this, new CalculatorArgs(result));
+        }
+    }
+
+    public class CalculatorArgs : EventArgs
+    {
+        public double answer;
+        public CalculatorArgs(double value)
+        {
+            answer = value;
+        }
+    }
+}
---
+++ Homework_6/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+    Доработайте класс калькулятора способным работать как с целочисленными так и с дробными числами. (возможно стоит задействовать перегрузку операций).
+*/
+
+using ConsoleApp1;
+
+Calculator calculator = new Calculator();
+calculator.ShowResult += PrintResult;
+while (true)
+{
+    Console.Write("Введите операцию: +, -, *, /.\n" +
+        "Для отмены крайнего действия напишите \"возврат\".\n" +
+        "Для завершения программы напишите \"отмена\" или введите пустую строку\t");
+    string? operation = Console.ReadLine();
+    if(operation == string.Empty || operation == "отмена")
+    {
+        Console.WriteLine("Завершение работы...");
+        break;
+    }
+    else if (operation == "возврат")
+    {
+        calculator.CancelLastCalculation();
+    }
+    else
+    {
+        Console.Write("Введите число: ");
+        string? number = Console.ReadLine();
+        calculator.Calculation(operation, number);
+    }
+    Console.WriteLine();
+}
+calculator.ShowResult -= PrintResult;
+
+
+void PrintResult(object? sender, CalculatorArgs e)
+{
+    Console.WriteLine($"Текущий результат равен = {e.answer}");
+}
